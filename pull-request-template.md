cf71cb97d22901a8dbbfade8acd24a6aab6fecf0
Kevin Bronsdijk <bronsdijk@bronsdijk.com>
2017-01-04 11:55:58 +0700

    Version 1
    Marked the current release as version 1
    Added support for .net 4.5
    Added PR template

---
+++ pull-request-template.md
@@ -0,0 +1,11 @@
+## Description
+A few sentences describing the overall goals of the pull request's commits.
+
+## Steps to Test or Reproduce
+Outline the steps to test or reproduce the PR here.
+
+## Todos
+- [ ] Tests succeeded
+- [ ] Documentation updated
+- [ ] Nuget release information updated (build.fsx)
+- [ ] Build version updated (build.bat)
--- src/gtmetrix-net/Properties/AssemblyInfo.cs
+++ src/gtmetrix-net/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("gtmetrix-net")]
 [assembly: AssemblyProductAttribute("gtmetrix-net")]
 [assembly: AssemblyDescriptionAttribute("GTmetrix-net .Net client")]
-[assembly: AssemblyVersionAttribute("0.0.1.9")]
-[assembly: AssemblyFileVersionAttribute("0.0.1.9")]
+[assembly: AssemblyVersionAttribute("0.1.0.0")]
+[assembly: AssemblyFileVersionAttribute("0.1.0.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "gtmetrix-net";
         internal const System.String AssemblyProduct = "gtmetrix-net";
         internal const System.String AssemblyDescription = "GTmetrix-net .Net client";
-        internal const System.String AssemblyVersion = "0.0.1.9";
-        internal const System.String AssemblyFileVersion = "0.0.1.9";
+        internal const System.String AssemblyVersion = "0.1.0.0";
+        internal const System.String AssemblyFileVersion = "0.1.0.0";
     }
 }
