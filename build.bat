2c599c129e33623ccd18c51822b30a316ad4352e
Duncan Edwards <duncan.edwards@vouchercloud.com>
2018-03-28 13:03:09 +0100

    Added the FullyLoadedTime property to the Results class.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 cls
 "tools\NuGet\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
-"tools\Fake\tools\Fake.exe" "build.fsx" version=1.0.1.0
+"tools\Fake\tools\Fake.exe" "build.fsx" version=1.0.2.0
 pause
